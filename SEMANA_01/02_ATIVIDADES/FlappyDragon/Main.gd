fb1d785cc39194d9eab0fc6d7411c7cbf38b8f50
heitorep <heitor.prudente@sou.inteli.edu.br>
2023-02-02 09:37:21 -0300

    Alteração da Velocidade

--- SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
+++ SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
@@ -1,9 +1,9 @@
 extends Node # instancia a classe Node2D
 
-var status = 1
-var vscore = 0
-var x = 1.5 
-var y = 1.5 
+var status = 0 #define se o personagem está parado ou jogando
+var vscore = 1 #responsável por zerar os pontos
+var x = 10 #velocidade do background e das coluna, e consequentemente do jogo
+var y = 1.5 #velocidade do personagem na vertical
 
 # executa essa função ao carregar o jogo
 func _ready():
@@ -41,11 +41,11 @@ func _process(delta):
 			
 		# se apertou seta para baixo, aumenta o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_down"):
-			$dragon.position.y += 2
+			$dragon.position.y += 8
 
 		# se apertou seta para cima, diminui o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_up"):
-			$dragon.position.y -= 4
+			$dragon.position.y -= 16
 			
 	elif status == 0: # parado
 		
